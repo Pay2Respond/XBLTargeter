2fb9232b3628029dbca839c21d8fbe752a6a84f6
Pay2Respond <pay2respond@gmail.com>
2025-05-29 01:12:44 -0400

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -2,23 +2,27 @@ import base64
 import socket
 import platform
 
+def obscure_secret():
+    return [81, 98, 122, 51, 83, 102, 116, 113, 112, 111, 101, 84, 102, 100, 115, 102, 117]
+
+def deobscure_secret(codes):
+    return "".join(chr(c - 1) for c in codes)
+
 def xor_encrypt_decrypt(data, key):
     return ''.join(chr(ord(c) ^ ord(key[i % len(key)])) for i, c in enumerate(data))
 
 def machine_fingerprint():
-    data = f"{socket.gethostname()}_{platform.node()}_{platform.machine()}"
-    return data
+    return f"{socket.gethostname()}_{platform.node()}_{platform.machine()}"
 
-def encode_key(machine_id, secret="Pay2RespondSecret"):
+def encode_key(machine_id):
+    secret = deobscure_secret(obscure_secret())
     xored = xor_encrypt_decrypt(machine_id, secret)
     return base64.urlsafe_b64encode(xored.encode()).decode()
 
 def generate_key():
     mf = machine_fingerprint()
     key = encode_key(mf)
-    print("Machine fingerprint:")
     print(mf)
-    print("\nGenerated Key:")
     print(key)
 
 if __name__ == "__main__":
